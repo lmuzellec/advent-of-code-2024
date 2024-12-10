bbcebaa3360d303f1a48fba128fe9ef8ff0219d4
Louis Muzellec <louis.muzellec@gmail.com>
2024-12-11 00:29:18 +0100

    day 10-1

---
+++ day-10/puzzle-1/graph.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+
+struct GraphNode;
+struct Connection
+{
+  GraphNode *ptr;
+};
+
+struct GraphNode
+{
+  int x;
+  int y;
+  int height;
+  std::vector<Connection> connections;
+  bool operator==(const GraphNode &other) const;
+};
+
+bool GraphNode::operator==(const GraphNode &other) const
+{
+  return x == other.x && y == other.y;
+}
+
+template <>
+struct std::hash<GraphNode>
+{
+  std::size_t operator()(const GraphNode &s) const noexcept
+  {
+    std::size_t h1 = std::hash<int>{}(s.x);
+    std::size_t h2 = std::hash<int>{}(s.y);
+    return h1 ^ (h2 << 1);
+  }
+};
