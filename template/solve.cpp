70ad3ded3553bb217ce68c3cccaed0bef1a96148
Louis Muzellec <louis.muzellec@gmail.com>
2024-12-12 00:00:46 +0100

    template

---
+++ template/solve.cpp
@@ -0,0 +1,30 @@
+#include <chrono>
+#include <vector>
+
+#include "utils.hpp"
+
+long solve(std::vector<std::string> input)
+{
+  long result = 0;
+
+  // TODO implement solve
+
+  return result;
+}
+
+int main()
+{
+  std::vector<std::string> input = utils::parseInput();
+
+  using milli = std::chrono::milliseconds;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  long long result = solve(input);
+
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::cout << "result: " << result << std::endl;
+
+  std::cout << "took "
+            << std::chrono::duration_cast<milli>(finish - start).count()
+            << " ms" << std::endl;
+}
